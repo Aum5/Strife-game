f3387fd4d793677953faa53c99cfedaeff166fb5
pate3993 <pate3993@sheridancollege.ca>
2023-02-13 18:14:53 -0500

    Student project version 3

--- src/strife/Game.java
+++ src/strife/Game.java
@@ -1,8 +1,24 @@
 package strife;
 
-public class Game {
+import java.util.Scanner;
+import java.util.function.Function;
+
+public class Game{
 
     public static void main(String[] args) {
+
+        Scanner k = new Scanner(System.in);
+
+        System.out.print("Enter Player 1 name");
+        String name1 = k.next();
+        
+        System.out.print("Enter Player 2 name");
+        String name2 = k.next();
+        
+        CardGenerator one = new CardGenerator();
+        Functions two = new Functions();
+        Player three = new Player();
+        Points four = new Points();
     }
     
 }
