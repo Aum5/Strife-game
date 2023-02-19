e1327ee660407df3a0119498d0a7a32d6bbc946b
Aum5 <122803737+Aum5@users.noreply.github.com>
2023-02-19 12:32:14 -0500

    Update Player.java

--- src/strife/Player.java
+++ src/strife/Player.java
@@ -3,7 +3,7 @@
  * Students can modify and extend to implement their game.
  * Add your name as an author and the date!
  */
-package strife;
+package ca.sheridancollege.project;
 
 /**
  * A class that models each Player in the game. Players have an identifier, which should be unique.
@@ -11,35 +11,24 @@ package strife;
  * @author dancye
  * @author Paul Bonenfant Jan 2020
  */
-public class Player {
+public abstract class Player {
 
-    private String name1; //the unique name for this player
-    private String name2;
+    private String name; //the unique name for this player
+
     /**
      * A constructor that allows you to set the player's unique ID
      *
      * @param name the unique ID to assign to this player.
      */
-    public Player(String name1, String name2) {
-        this.name1 = name1;
-        this.name2 = name2;
+    public Player(String name) {
+        this.name = name;
     }
-
-    Player() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-    
 
     /**
      * @return the player name
      */
-    public String getName1() {
-        return name1;     
-    }
-    
-    
-    public String getName2() {
-        return name2;     
+    public String getName() {
+        return name;
     }
 
     /**
@@ -47,17 +36,14 @@ public class Player {
      *
      * @param name the player name to set
      */
-    public void setName1(String name1) {
-        this.name1 = name1;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    
-    public void setName2(String name2) {
-        this.name2 = name2;
-    }
     /**
      * The method to be overridden when you subclass the Player class with your specific type of Player and filled in
      * with logic to play your game.
-     */
+     */
+    public abstract void play();
 
 }
