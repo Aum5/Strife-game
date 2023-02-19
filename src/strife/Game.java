7dbe02e4d00fe50bb211c1315b2224c19564cbe8
Aum5 <122803737+Aum5@users.noreply.github.com>
2023-02-19 12:27:11 -0500

    Update Game.java

--- src/strife/Game.java
+++ src/strife/Game.java
@@ -1,24 +1,58 @@
-package strife;
+/**
+ * SYST 17796 Project Base code.
+ * Students can modify and extend to implement their game.
+ * Add your name as an author and the date!
+ */
+package ca.sheridancollege.project;
 
-import java.util.Scanner;
-import java.util.function.Function;
+import java.util.ArrayList;
 
-public class Game{
+/**
+ * The class that models your game. You should create a more specific child of this class and instantiate the methods
+ * given.
+ *
+ * @author dancye
+ * @author Paul Bonenfant Jan 2020
+ */
+public abstract class Game {
 
-    public static void main(String[] args) {
+    private final String name;//the title of the game
+    private ArrayList<Player> players;// the players of the game
 
-        Scanner k = new Scanner(System.in);
-
-        System.out.print("Enter Player 1 name");
-        String name1 = k.next();
-        
-        System.out.print("Enter Player 2 name");
-        String name2 = k.next();
-        
-        CardGenerator one = new CardGenerator();
-        Functions two = new Functions();
-        Player three = new Player();
-        Points four = new Points();
+    public Game(String name) {
+        this.name = name;
+        players = new ArrayList();
     }
-    
-}
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @return the players of this game
+     */
+    public ArrayList<Player> getPlayers() {
+        return players;
+    }
+
+    /**
+     * @param players the players of this game
+     */
+    public void setPlayers(ArrayList<Player> players) {
+        this.players = players;
+    }
+
+    /**
+     * Play the game. This might be one method or many method calls depending on your game.
+     */
+    public abstract void play();
+
+    /**
+     * When the game is over, use this method to declare and display a winning player.
+     */
+    public abstract void declareWinner();
+
+}//end class
